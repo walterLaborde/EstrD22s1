694b3f8155abadd3851f1133d6e85415c58bceb3
Walter <walter.laborde@hotmail.com>
2022-04-08 21:39:59 -0300

    longitud

--- Practica_2_Listas_Y_Recursion_Estructural.hs
+++ Practica_2_Listas_Y_Recursion_Estructural.hs
@@ -6,3 +6,9 @@
 sumatoria :: [Int] -> Int
 sumatoria []     = 0
 sumatoria (n:ns) = n + sumatoria ns
+
+-- 2. longitud
+
+longitud :: [a] -> Int
+longitud []     = 0
+longitud (n:ns) = 1 + longitud ns
