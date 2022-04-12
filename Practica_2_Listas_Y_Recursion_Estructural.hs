881c94f4661107a6cd8438511c46653115897b75
Walter <walter.laborde@hotmail.com>
2022-04-12 15:31:55 -0300

    #4 cuenta regresiva contempla negativos

--- Practica_2_Listas_Y_Recursion_Estructural.hs
+++ Practica_2_Listas_Y_Recursion_Estructural.hs
@@ -118,8 +118,9 @@ factorial n = n * factorial (n-1)
 -- 2. cuentaRegresiva
 
 cuentaRegresiva :: Int -> [Int]
-cuentaRegresiva 0 = []
-cuentaRegresiva n = n : cuentaRegresiva(n-1)
+cuentaRegresiva n = if n<1
+                        then []
+                        else n : cuentaRegresiva(n-1)
 
 -- 3. repetir
 
