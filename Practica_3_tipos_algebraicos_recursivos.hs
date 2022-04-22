9720c624088ec9f43b77c44354e2ccdf22f733fd
Walter <walter.laborde@hotmail.com>
2022-04-21 21:58:02 -0300

    nroBolitas

--- Practica_3_tipos_algebraicos_recursivos.hs
+++ Practica_3_tipos_algebraicos_recursivos.hs
@@ -3,3 +3,36 @@
 -- TIPOS RECURSIVOS SIMPLES
 
 -- 1.1. Celdas con bolitas
+
+data Color = Azul | Rojo 
+data Celda = Bolita Color Celda | CeldaVacia
+
+-- casos de uso
+
+dosRojasDosAzules = Bolita Rojo 
+                        (Bolita Azul 
+                            (Bolita Rojo 
+                                (Bolita Azul CeldaVacia)))
+celda0 = CeldaVacia
+celda1 = Bolita Rojo CeldaVacia
+celda2 = Bolita Rojo (Bolita Azul CeldaVacia)
+celda3 = Bolita Rojo (Bolita Rojo CeldaVacia)
+
+-- nroBolitas
+
+nroBolitas :: Color -> Celda -> Int
+
+-- nroBolitas colour CeldaVacia     = ...
+-- nroBolitas colour Bolita col cel = ... col .. nroBolitas cel 
+--
+nroBolitas _      CeldaVacia       = 0
+nroBolitas colour (Bolita col cel) = unoSi(coloresIguales colour col) + nroBolitas colour cel 
+
+coloresIguales :: Color -> Color -> Bool
+coloresIguales Azul  Azul  = True
+coloresIguales Rojo  Rojo  = True
+coloresIguales c1    c2    = False
+
+unoSi :: Bool -> Int
+unoSi True  = 1
+unoSi False = 0
