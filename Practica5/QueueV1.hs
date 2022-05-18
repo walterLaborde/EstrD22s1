ba0b9832c371a145a6cd55e4f425ccd1fd8f77ce
Walter <walter.laborde@hotmail.com>
2022-05-18 10:19:21 -0300

    queueV1 resto de funciones

--- Practica5/QueueV1.hs
+++ Practica5/QueueV1.hs
@@ -22,3 +22,35 @@ Inv.Rep.:
 emptyQ :: Queue a -- O(1)
 emptyQ = Q []
 -- Crea una cola vacía.
+
+isEmptyQ :: Queue a -> Bool -- O(1)
+isEmptyQ (Q xs) = null' xs
+
+null' :: [a] -> Bool
+null' [] = True
+null' _ = False
+-- Dada una cola indica si la cola está vacía.
+
+queue :: a -> Queue a -> Queue a -- O(1) 
+queue y (Q xs) = Q (agregarAlFinal xs y)
+
+agregarAlFinal :: [a] -> a -> [a]
+agregarAlFinal [] e     = [e]
+agregarAlFinal (x:xs) e = x : agregarAlFinal xs e
+-- Dados un elemento y una cola, agrega ese elemento a la cola.
+
+firstQ :: Queue a -> a -- O(1)
+firstQ (Q xs) = head' xs
+
+head' :: [a] -> a
+head' (x:_) = x
+head' _ = error "head no se puede usar con []"
+-- Dada una cola devuelve el primer elemento de la cola.
+
+dequeue :: Queue a -> Queue a -- O(1)
+dequeue (Q xs) = Q (tail' xs)
+
+tail' :: [a] -> [a]
+tail' (_:xs) = xs
+tail' _      = error "tail no se puede usar con []"
+-- Dada una cola la devuelve sin su primer elemento.
