2256b2a6a7b28734e5bcbdfe0d604ed9d0d6fcb1
Walter <walter.laborde@hotmail.com>
2022-06-14 19:52:13 -0300

    practica9 ejercisio 3 cpp

---
+++ Practica9/Par.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include "Par.h"
+using namespace std;
+
+// Propósito: construye un par
+Par consPar(int x, int y) {
+    Par p;
+    p.x = x;
+    p.y = y;
+    return(p);
+}
+
+// Propósito: devuelve la primera componente
+int fst(Par p) {
+    return(p.x);
+}
+
+// Propósito: devuelve la segunda componente
+int snd(Par p) {
+    return(p.y);
+}
+
+// Propósito: devuelve la mayor componente
+int maxDelPar(Par p) {
+    if(p.x > p.y) {
+        return p.x;
+    } 
+    else {
+        return p.y;
+    }
+}
+
+// Propósito: devuelve un par con las componentes intercambiadas
+Par swap(Par p) {
+    Par sPar;
+    sPar.x = p.y;
+    sPar.y = p.x;
+    return(sPar);
+}
+
+// Propósito: devuelve un par donde la primer componente
+// es la división y la segunda el resto entre ambos números
+Par divisionYResto(int n, int m) {
+    Par divRest;
+    divRest.x = n / m;
+    divRest.y = n % m;
+    return(divRest);
+}
+
+
+int main() {
+    Par parNuevo = consPar(3,2);
+    cout << parNuevo << endl;
+}
+
--- practica09.cpp
+++ practica09.cpp
@@ -1,10 +1,44 @@
-# include <iostream>
+#include <iostream>
 
 using namespace std;
 
-int main() {
-    int  x = 17;
-    char c = 'a';
-    cout << x << endl;
-    cout << c << endl;
+// proposito: Imprimir el factorial de n.
+// Precondición: n >= 0
+int fc(int n) {    // 3
+int x = 1;         // 1     3 ,       6 ,
+while(n > 0) {     // 3     2 ,       1 , 
+x = x * n;         // 1*3 = 3 , 3*2 = 6 , 
+n--;               // 3-1 = 2 , 2-1 = 1 ,
 }
+return x;
+}
+
+
+int main() {
+    int  x = 8;
+    int res = fc(x);
+    cout << res << endl;
+    //cout << c << endl;
+}
+
+
+// Proposito: Imprimir los caracteres consecutivos desde c1 hasta c2 inclusive.
+// ejemplos de resultado: c1 = 'a', c2 = 'c' .... a, b, c
+// Precondición: c1 < c2
+void printFromTo(char c1, char c2) {
+for(int i = 0; c1 + i <= c2; i++) {
+cout << c1 + i << ", ";
+}
+cout << endl;
+}
+
+// 
+// Precondición: n <= m
+int ft(int n, int m) {     // 2, 4
+if (n == m) {              // false
+return n;                   
+}
+return n + ft(n+1, m);     // 2 + (3 + 4)  
+}
+
+
