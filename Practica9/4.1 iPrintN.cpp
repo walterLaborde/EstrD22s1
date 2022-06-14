016b528171c0e25834fd90646ddd52724d99256b
Walter <walter.laborde@hotmail.com>
2022-06-14 20:32:13 -0300

    4.1 iPrintN

---
+++ Practica9/4.1 iPrintN.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+//Propósito: imprime n veces un string s.
+void printN(int n, string s) {
+    string repetirNVeces;
+    for(int i = 0; i < n; i++) {
+        repetirNVeces += s;
+    }
+}
+
+int main() {
+
+}
